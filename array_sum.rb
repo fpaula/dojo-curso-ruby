1fe552825958cb0bc7cbda409aa1f60eea0c8508
Fred de Paula <frederico.paula@youse.com.br>
2018-08-15 09:35:07 -0300

    Assert and first exercise

---
+++ array_sum.rb
@@ -0,0 +1,9 @@
+require 'colorize'
+require_relative 'assert'
+
+def sum(numbers)
+  6
+end
+
+
+assert(sum([1, 2, 3]), 6)
---
+++ assert.rb
@@ -0,0 +1,7 @@
+def assert(result, expected_result)
+  if result == expected_result
+    puts ".".green
+  else
+    puts "F".red
+  end
+end
