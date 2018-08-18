0292c539c2fc63361620e35222d9372ced2acb1b
Artur <artur.prado@gmail.com>
2018-08-18 13:12:37 -0300

    Fix wrong test

--- array_sum.rb
+++ array_sum.rb
@@ -7,8 +7,8 @@ end
 
 assert(sum([1]), 1)
 assert(sum([1, 2, 3]), 6)
-assert(sum([9, 8, 7]), 9)
-assert(sum([1, 12, 3]), 12)
+assert(sum([9, 8, 7]), 24)
+assert(sum([1, 12, 3]), 16)
 assert(sum([]), nil)
-assert(sum([1, 21, 12, 3, 0]), 21)
-assert(sum([1, 3, 3]), 3)
+assert(sum([1, 21, 12, 3, 0]), 37)
+assert(sum([1, 3, 3]), 7)
--- data_type.rb
+++ data_type.rb
@@ -7,6 +7,6 @@ end
 assert(type([1]), [Fixnum])
 assert(type([1.8]), [Float])
 assert(type(['Text']), [String])
-assert(type([nil]), NilClass)
+assert(type([nil]), [NilClass])
 assert(type([]), nil)
 assert(type([1, 'text', nil, 12.5]), [Fixnum, String, NilClass, Float])
